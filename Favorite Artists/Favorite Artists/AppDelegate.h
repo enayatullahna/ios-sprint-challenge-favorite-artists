4ba426a62faa5a97641706eb698654bc1a7a361f
Enayatullah Naseri <enayatullahna@gmail.com>
2020-04-18 09:12:29 -0700

    Started the the Xcode project 'Favorite Artists'.

---
+++ Favorite Artists/Favorite Artists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Favorite Artists
+//
+//  Created by Enayatullah Naseri on 4/18/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Favorite Artists/Favorite Artists/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Favorite Artists
+//
+//  Created by Enayatullah Naseri on 4/18/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
