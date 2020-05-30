92f91b71b65d8db3884fe2f3c8555c4ae3fc06f5
Enayatullah Naseri <enayatullahna@gmail.com>
2020-05-30 08:57:16 -0700

    Done with model

---
+++ FavoriteArtists/FavoriteArtists/LSIArtist.h
@@ -0,0 +1,22 @@
+//
+//  LSIArtist.h
+//  FavoriteArtists
+//
+//  Created by Enayatullah Naseri on 5/30/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIArtist : NSObject
+
+//MARK:- Properties
+@property NSString *name;
+@property NSString *biography;
+@property int yearFormed;
+
+@end
+
+NS_ASSUME_NONNULL_END
